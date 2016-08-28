b09a7817bea18c80e856730f592f02b72d813322
KowsalyaE <kowsalyahanu@gmail.com>
2016-08-28 09:39:56 +0530

    Add files via upload

---
+++ eo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class eo
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n%2==0)
+{
+System.out.println("even");
+}
+if(n%2!=0)
+{
+System.out.println("odd");
+}
+}
+}
